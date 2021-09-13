41b0d6c652e116d0bc4592240048297b707ad056
anthony <anthonydwan@outlook.com>
2021-09-14 00:19:01 +1000

    working files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'regenerator-runtime/runtime';
 const systemModule = (() => {
     async function getWeather(location){
         try{
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=50f2bb6a8e9ef7686c7837c2f0961653`,
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid={KEY}`,
             {mode: 'cors'})
             const weatherData = await response.json()
             return weatherData
